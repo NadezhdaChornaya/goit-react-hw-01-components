230a62cfb4756e6c7a5a9b13fa361dbfdb14515d
Nadezhda Chornaya <nadezhdachornaja@gmail.com>
2021-01-11 23:21:27 +0200

    aadded git-hub pages

--- src/components/App.js
+++ src/components/App.js
@@ -18,10 +18,10 @@ export default function App() {
                 avatar={user.avatar}
                 stats={user.stats}
             />
-            <Statistics title="Upload stats" stats={statisticalData} />;
-            <Statistics stats={statisticalData} />;
-            <FriendList friends={friends} />;
-            <TransactionHistory items={transactions} />;
+            <Statistics title="Upload stats" stats={statisticalData} />
+            <Statistics stats={statisticalData} />
+            <FriendList friends={friends} />
+            <TransactionHistory items={transactions} />
         </>
     )
 }
